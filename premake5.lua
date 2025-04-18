2c84f123ea97f41fa956e60665cc808be41607d2
Ashton Dorough <keithlekgolo@gmail.com>
2025-04-18 07:50:39 -0400

    Update
    Added SFML, ImGui, & Premake
    Basic Project Setup

---
+++ premake5.lua
@@ -0,0 +1,84 @@
+workspace "Card Lab"
+    architecture "x64"
+    configurations { "Debug", "Release" }
+    startproject "Card Lab"
+	location "Build"
+
+project "Card Lab"
+    location "Build"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    systemversion "latest"
+	targetname "Enigma's Card Lab"
+
+    targetdir ("Build/bin/%{cfg.architecture}/%{cfg.buildcfg}")
+    objdir ("Build/obj/%{cfg.architecture}/%{cfg.buildcfg}")
+
+    files
+	{
+        "Source/**.h",
+        "Source/**.cpp"
+    }
+	
+	excludes
+	{
+		"Vendor/imgui/backends/**.cpp",
+		"Vendor/imgui/backends/**.h",
+		"Vendor/imgui/examples/**.cpp",
+		"Vendor/imgui/examples/**.h",
+		"Vendor/imgui/misc/**.cpp",
+		"Vendor/imgui/misc/**.h",
+        "Vendor/SFML/examples/**.cpp",
+        "Vendor/SFML/examples/**.h"
+    }
+
+    includedirs
+	{
+        "Vendor/imgui",
+        "Vendor/SFML/include"
+    }
+
+    libdirs
+	{
+        "Vendor/SFML/lib"
+    }
+
+    links
+	{
+        "opengl32",
+        "freetype",
+        "winmm",
+        "gdi32",
+        "flac",
+        "vorbisenc",
+        "vorbisfile",
+        "vorbis",
+        "ogg",
+        "ws2_32",
+    }
+
+    filter "system:windows"
+        defines { "PLATFORM_WINDOWS", "SFML_STATIC" }
+
+    filter "configurations:Debug"
+        defines { "DEBUG" }
+        symbols "On"
+		
+		links
+		{
+		    "sfml-graphics-s-d",
+			"sfml-window-s-d",
+			"sfml-system-s-d",
+		}
+
+    filter "configurations:Release"
+        defines { "NDEBUG" }
+        optimize "On"
+
+		links
+		{
+		    "sfml-graphics-s",
+			"sfml-window-s",
+			"sfml-system-s",
+		}
